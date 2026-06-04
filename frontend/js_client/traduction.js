ce8bf074f276866861313a6f091e7bff7b34538f
Antoine VIVIEN <antoinev205@gmail.com>
2026-06-04 18:53:20 +0200

    implemantation of the traduction system

---
+++ frontend/js_client/traduction.js
@@ -0,0 +1,11 @@
+// fichiers: traductions.js
+window.traductions = {
+    french: {
+        home: "Menu d'accueil",
+        settings: "PARAMÈTRES"
+    },
+    english: {
+        home: "Home Menu",
+        settings: "SETTINGS"
+    }
+};
